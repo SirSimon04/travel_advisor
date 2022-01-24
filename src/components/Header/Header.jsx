6d3fb2e0216d119edf51e60e52bb76d75eb6500b
SirSimon04 <simi@engelnetz.de>
2022-01-24 18:30:05 +0100

    add navbar and map layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Autocomplete } from "@react-google-maps/api";
 import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
-import { SearchIcon } from "@material-ui/icons";
+import SearchIcon from "@material-ui/icons/Search";
+
+import useStyles from "./styles"
 
 function Header() {
+  const classes = useStyles();
   return (
     <AppBar position="static">
       <Toolbar className={classes.toolbar}>
@@ -14,14 +17,14 @@ function Header() {
           <Typography variant="h6" className={classes.title}>
             Explore new Places
           </Typography>
-          <Autocomplete>
+          {/* <Autocomplete> */}
             <div className={classes.search}>
               <div className={classes.searchIcon}>
                 <SearchIcon />
               </div>
               <InputBase placeholder="Search..." classes={{root:classes.inputRoot, input: classes.inputInput}}/>
             </div>
-          </Autocomplete>
+          {/* </Autocomplete> */}
         </Box>
       </Toolbar>
     </AppBar>
